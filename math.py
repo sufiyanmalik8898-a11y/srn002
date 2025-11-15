aaa0e5bf01447aa26be5594aba554f25924b39a4
sufiyanmalik8898-a11y <sufiyanmalik8898@gmail.com>
2025-11-15 08:52:15 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,13 @@
+try:
+  a=float(input("enter the first number:"))
+  b=flaot(input("enter the second number:"))
+  print(f"addition:{a+b}")
+  print(f"substraction:{a-b}")
+except ValueError:
+  print("please enter the valid numbers.")
+
+
+
+
+
+
